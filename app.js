bb60dfeaa626098ecdf561d27a4a2769b2b49600
Jared Candelaria <jaredcandelaria@Jareds-MacBook-Pro.local>
2018-09-11 09:26:14 -0600

    basic route check

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
-const app = express
+const app = express()
 const port = process.env.PORT || 3000
+const listener = () => `Listening on port ${port}!`
 
+app.listen(port, listener)
 
+app.get('/', (req, res) => {
+    res.send('The Route Works!')
+})
 
 console.log(`listening on ${port}`)
